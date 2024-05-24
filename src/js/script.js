e14ae381a00c40626adf5f7a98489db70731164d
Rustam <drbtrmr@gmail.com>
2024-05-24 12:29:52 +0500

    add jquery validate and css styles for error class

--- src/js/script.js
+++ src/js/script.js
@@ -66,4 +66,65 @@ $(document).ready(function(){
             $('.overlay, #order').fadeIn('slow');
         });
     });
+
+    // Jquery.validate
+    // $('#consultation-form').validate();
+    // $('#consultation form').validate({
+    //     rules: {
+    //         name: {
+    //             required: true,
+    //             minlength: 2
+    //           },
+    //         phone: "required",
+    //         email: {
+    //             required: true,
+    //             email: true
+    //         }
+    //     },
+    //     messages: {
+    //         name: {
+    //             required: "Пожалуйста, введите своё имя",
+    //             minlength: jQuery.validator.format("Введите {0} символа!")
+    //           },
+    //         phone: "Пожалуйста, введите свой номер телефона",
+    //         email: {
+    //           required: "Пожалуйста, введите свою почту",
+    //           email: "Неправильно введен адрес почты"
+    //         }
+    //       }
+    // });
+    // $('#order form').validate();
+
+    function validateForms(form) {
+        $(form).validate({
+            rules: {
+                name: {
+                    required: true,
+                    minlength: 2
+                  },
+                phone: "required",
+                email: {
+                    required: true,
+                    email: true
+                }
+            },
+            messages: {
+                name: {
+                    required: "Пожалуйста, введите своё имя",
+                    minlength: jQuery.validator.format("Введите {0} символа!")
+                  },
+                phone: "Пожалуйста, введите свой номер телефона",
+                email: {
+                  required: "Пожалуйста, введите свою почту",
+                  email: "Неправильно введен адрес почты"
+                }
+              }
+        });
+    };
+
+    validateForms('#consultation-form');
+    validateForms('#consultation form');
+    validateForms('#order form');
 });
+
+
